ff77fea092ed20c1130691237dfa9e36425a8426
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2016-07-27 18:03:48 +0900

    rev minoro

--- latex2hiki.gemspec
+++ latex2hiki.gemspec
@@ -31,4 +31,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "minitest"
   spec.add_development_dependency "yard"
+  spec.add_development_dependency "hiki2md"
+  spec.add_development_dependency "mathjax-yard"
 end
--- lib/latex2hiki.rb
+++ lib/latex2hiki.rb
@@ -2,6 +2,7 @@ require 'optparse'
 require "latex2hiki/version"
 #require "latex2hiki/latex2hiki"
 require "latex2hiki/latex2hiki_new"
+require 'fileutils'
 
 module Latex2hiki
   # Your code goes here...
@@ -23,10 +24,43 @@ module Latex2hiki
           opt.version = Latex2hiki::VERSION
           puts opt.ver
         }
+        opt.on('--init [NAME]','initialize NAME directory.') { |name|
+          name = name || './'
+          init(name)
+          exit
+        }
+        opt.on('--figures [NAME]','gather NAME directory.') { |name|
+          name = name || './'
+          gather_figures(name)
+          exit
+        }
       end
       command_parser.parse!(@argv)
       puts NKF.nkf("-w",Latex.new(File.read(ARGV[0])).to_hiki)
       exit
+    end
+
+    def gather_figures(name)
+      file_names =File.join(name,'**','*.eps')
+      files = Dir.glob(file_names)
+      target = File.join(name,'figures')
+      files.each{|file|
+        FileUtils.cp(file,target,:verbose=>true)
+      }
+    end
+
+    def init(name)
+      puts name
+      if Dir.entries('.').include?(name)
+        print "#{name} exists. rm -rf first.\n"
+        exit
+      else
+        system "bundle gem #{name}"
+      end
+      ['lib','bin','spec'].each{|dir|
+        FileUtils.rm_r(File.join(name,dir),:verbose=>true)
+      }
+      FileUtils.mkdir(File.join(name,'figures'),:verbose=>true)
     end
   end
 end
