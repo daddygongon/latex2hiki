02dfad67bc21fff065c02cb18033082766cb3baf
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2016-07-27 10:57:12 +0900

    before Maple

--- lib/latex2hiki/latex2hiki_new.rb
+++ lib/latex2hiki/latex2hiki_new.rb
@@ -1,35 +1,38 @@
 # -*- coding: utf-8 -*-
+require 'kconv'
 
 class Latex < String
 
-  def initialize(output)
+  def initialize(input)
+    @input = input
+    @f = ""
   end
 
-  def to_hiki(text)
-  section = []
-  text.split("\n").each do |line|
-    case line
-    when /^Start(\w+)/
-      section.push $1
-      next
-    when /^End(\w+)/
-      section.pop
-      next
-    end
-    p section
+  def to_hiki
+    section = []
+    @input.split("\n").each do |line|
+      case line
+      when /^\\(\w+)/
+        section.push $1
+        next
+      when /^End(\w+)/
+        section.pop
+        next
+      end
+      p section[-1]
 
-    case section
-    when ["CharMetrics"]
-#      next unless line =~ /^CH?\s/
-      p line
-      p name           = line[/\bN\s+(\.?\w+)\s*;/, 1]
-      p glyp_width     = line[/\bWX\s+(\d+)\s*;/, 1].to_i
-      p bounding_boxes = line[/\bB\s+([^;]+);/, 1].to_s.rstrip
-    else
-      p line
+      case section[-1]
+      when ["author"]
+        @f << line
+        cont = line.match(/\\(\w+)\{(\w+)\}/)
+        p cont
+        @f <<  cont
+        @f << cont[2]
+        exit
+      else
+#        @f << section[-1]
+      end
     end
-  end
-end
-
+    return @f
   end
 end
